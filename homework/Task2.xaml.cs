390d975d7a7d35f7f3ef9aaed68f2d30e046cba9
RiderGraffity <efomin439@gmail.com>
2026-04-23 03:59:56 +0300

    Add files via upload

---
+++ homework/Task2.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Wpf1
+{
+    public partial class Task2 : Window
+    {
+        public Task2()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ homework/Task4.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Wpf1
+{
+      
+    public partial class Task4 : Window
+    {
+        public Task4()
+        {
+            InitializeComponent();
+        }
+    }
+}
